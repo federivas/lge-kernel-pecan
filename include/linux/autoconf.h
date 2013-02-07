156e4335f9d2f270381b4935b0404d9973407fea
Mike Lockwood <lockwood@android.com>
2013-02-07 16:56:41 +0530

    USB: composite: Add usb_composite_force_reset utility to force enumer… …ation
    Use this rather than calling usb_gadget_disconnect and usb_gadget_connect
    directly to avoid sending USB disconnect events to userspace when resetting
    the bus.
    
    Change-Id: If37a81c096a5725222c75ac9ba3f79b605b28b6b
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- include/linux/autoconf.h
+++ include/linux/autoconf.h
@@ -1,7 +1,7 @@
 /*
  * Automatically generated C config: don't edit
  * Linux kernel version: 2.6.32.9
- * Thu Feb  7 16:28:24 2013
+ * Thu Feb  7 16:55:34 2013
  */
 #define AUTOCONF_INCLUDED
 #define CONFIG_VIDEO_V4L1_COMPAT 1
